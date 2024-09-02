d64f61040fef7b36fa7ab9e9dceb292dac88d0c9
wali55 <walisantunu@gmail.com>
2024-09-02 17:31:37 +0600

    login page with mui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const router = createBrowserRouter([
   {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,103 @@
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/images/samarabiz-logo.svg";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  Grid2,
+  Typography,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  Link,
+} from "@mui/material";
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  const handleLogin = () => {};
+
+  return (
+    <div>
+      {/* Header Section */}
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "#3A1078", padding: "8px 16px" }}
+      >
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box
+            component="img"
+            src={Logo}
+            alt="SamaraBiz Logo"
+            sx={{ height: 60 }}
+          />
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ backgroundColor: "white", color: "black" }}
+            onClick={handleHomeClick}
+          >
+            Back to Home
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      {/* Form Section */}
+      <Grid2
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ height: "80vh" }}
+      >
+        <Box component="form" sx={{ width: 300, textAlign: "center" }}>
+          <Typography variant="h5" gutterBottom>
+            Login to SamaraBiz
+          </Typography>
+          <TextField
+            label="Email/Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Remember Me"
+            sx={{ textAlign: "left", marginRight: 20 }}
+          />
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ marginTop: 2, backgroundColor: "#3A1078" }}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <Grid2 container justifyContent="center" sx={{ marginTop: 2 }}>
+            <Link href="#" variant="body2">
+              Forgot Password?
+            </Link>
+            <Link href="/register" variant="body2" sx={{ marginTop: 1 }}>
+              Don't have an account? Register
+            </Link>
+          </Grid2>
+        </Box>
+      </Grid2>
+    </div>
+  );
 };
 
 export default Login;

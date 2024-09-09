1b6b9429e3dd908b0fdc5101a2ab0c59e8494a20
wali55 <walisantunu@gmail.com>
2024-09-09 22:37:37 +0600

    menu items

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -58,6 +58,67 @@ const AdminDashboard = () => {
             <MenuItem onClick={() => navigate('/admin/projects/expenses')}>Project Expenses</MenuItem>
             <MenuItem onClick={() => navigate('/admin/projects/archive')}>Archive Projects</MenuItem>
           </Menu>
+
+          {/* Investors Menu */}
+          <Button
+            color="inherit"
+            onClick={openMenu ? () => handleMenuClose : (e) => handleMenuOpen(e, 'investors')}
+          >
+            Investors
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={openMenu === 'investors'}
+            onClose={handleMenuClose}
+            MenuListProps={{onClick: handleMenuClose}}
+          >
+            <MenuItem onClick={() => navigate('/admin/investors/all')}>All Investors</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/investors/invested-projects')}>Invested Projects</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/investors/investment-agreement')}>Investment Agreement</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/investors/deposit-agreement')}>Deposit Agreement</MenuItem>
+          </Menu>
+
+          {/* Financial details menu */}
+          <Button
+            color="inherit"
+            onClick={openMenu ? () => handleMenuClose() : (e) => handleMenuOpen(e, 'financial')}
+          >
+            Financial Details
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={openMenu === 'financial'}
+            onClose={handleMenuClose}
+            MenuListProps={{onClick: handleMenuClose}}
+          >
+            <MenuItem onClick={() => navigate('/admin/financial/profit-details')}>Profit Details</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/financial/withdraw-details')}>Withdraw Details</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/financial/deposit-details')}>Deposit Details</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/financial/platform-profit')}>Platform Profit</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/financial/platform-withdraw')}>Platform Withdraw Details</MenuItem>
+          </Menu>
+
+          {/* Company */}
+          <Button
+            color="inherit"
+            onClick={openMenu ? () => handleMenuClose() : (e) => handleMenuOpen(e, 'company')}
+          >
+            Company
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={openMenu === 'company'}
+            onClose={handleMenuClose}
+            MenuListProps={{onClick: handleMenuClose}}
+          >
+            <MenuItem onClick={() => navigate('/admin/company/departments')}>Departments</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/faq')}>FAQ</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/camp')}>Camp</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/general')}>General Settings</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/company-info')}>Company Info Settings</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/currency-master')}>Currency Master</MenuItem>
+            <MenuItem onClick={() => navigate('/admin/company/conversion-history')}>Conversion History</MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>

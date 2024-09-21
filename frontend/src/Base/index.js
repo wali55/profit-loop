bb66fc551f4977a94ec63144c4d680314eb04291
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2024-09-21 11:58:56 +0600

    Refactor login API endpoint URL to use production server

--- frontend/src/Base/index.js
+++ frontend/src/Base/index.js
@@ -1,3 +1,3 @@
 // https://samaraiz-node-backend.onrender.com/api/v1
 // http://localhost:4000/api/v1
-export const baseUrl = "http://localhost:4000/api/v1";
+export const baseUrl = "https://samaraiz-node-backend.onrender.com/api/v1";

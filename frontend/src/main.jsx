fcfbe3ed92205212d9637488a3cdb84169aee933
wali55 <walisantunu@gmail.com>
2024-09-27 09:34:07 +0600

    confirmation dialog

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,17 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+// common
 import Home from "./pages/common/Home.jsx";
 import Login from "./pages/common/Login.jsx";
-import Register from "./pages/investor/Register.jsx";
+// admin
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import AdminDepositRequests from "./pages/admin/AdminDepositRequests.jsx";
+import AdminWithdrawRequests from "./pages/admin/AdminWithdrawRequests.jsx";
+// investors
+import Register from "./pages/investor/Register.jsx";
 import InvestorDashboard from "./pages/investor/InvestorDashboard.jsx";
 import InvestorDepositDetails from "./pages/investor/InvestorDepositDetails.jsx";
-import AdminDepositRequests from "./pages/admin/AdminDepositRequests.jsx";
 import InvestorsBanks from "./pages/investor/InvestorsBanks.jsx";
 import InvestorWithdrawDetails from "./pages/investor/InvestorWithdrawDetails.jsx";
 // redux
@@ -37,6 +41,10 @@ const router = createBrowserRouter([
   {
     path: "/admin/requests/deposit-requests",
     element: <AdminDepositRequests />,
+  },
+  {
+    path: "/admin/requests/withdraw-requests",
+    element: <AdminWithdrawRequests />,
   },
   // investor
   {
--- frontend/src/pages/admin/AdminWithdrawRequests.jsx
+++ frontend/src/pages/admin/AdminWithdrawRequests.jsx
@@ -14,31 +14,36 @@ import {
   Radio,
   Box,
   Button,
-  Typography
+  Typography,
 } from "@mui/material";
 import Footer from "../../components/common/Footer";
 import { baseUrl } from "../../Base";
 
-const AdminDepositRequests = () => {
+const AdminWithdrawRequests = () => {
   // retrieve token from localStorage
   const token = localStorage.getItem("token");
-
-  // deposit data
-  const [depositData, setDepositData] = useState([]);
-  // deposit status
-  const [status, setStatus] = useState("pending"); // default to pending
-  //loading
+  // withdraw data
+  const [withdrawData, setWithdrawData] = useState([]);
+  // withdraw status
+  const [status, setStatus] = useState("pending");
+  // loading
   const [loading, setLoading] = useState(true);
   // pagination
   const [page, setPage] = useState(0); // current page number (0 indexed)
   const [rowsPerPage, setRowsPerPage] = useState(5); // number of rows per page
   const [totalRows, setTotalRows] = useState(0); // total number of rows from API
+  // dialog
+  const [open, setOpen] = useState(false);
 
-  // fetch deposit data function
-  const fetchDepositData = async () => {
+  // handle open/close dialog (of confirmation status change)
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // fetch withdraw data
+  const fetchWithdrawData = async () => {
     try {
       const response = await fetch(
-        `${baseUrl}/deposit?pagination=true&pageNumber=${
+        `${baseUrl}/withdraw_amount?pagination=true&pageNumber=${
           page + 1
         }&rowPerPage=${rowsPerPage}&status=${status}`,
         {
@@ -47,24 +52,24 @@ const AdminDepositRequests = () => {
             "Content-Type": "application/json",
             authorization: token,
           },
-          credentials: "include"
+          credentials: "include",
         }
       );
 
-      if (!response.ok) throw new Error("Failed to get the deposit data");
+      if (!response.ok) throw new Error("Failed to get the withdraw data");
 
       const data = await response.json();
-      setDepositData(data?.data?.results || []); // deposit data
-      setTotalRows(data?.data?.count || 0); // total row count from api
-    } catch (error) {
-      console.error("Error when fetching deposit data", error);
+
+      setWithdrawData(data?.data?.results || []);
+      setTotalRows(data?.data?.count || 0);
+    } catch (err) {
+      console.error("Error", err);
     }
   };
 
-  // get all deposit data of the user
+  // fetch all withdraw data when loading the page
   useEffect(() => {
-    if (!token) return;
-    fetchDepositData();
+    fetchWithdrawData();
     setLoading(false);
   }, [status, page, rowsPerPage]);
 
@@ -84,11 +89,16 @@ const AdminDepositRequests = () => {
     setStatus(e.target.value);
   };
 
+  // handle confirm status change (approve withdraw request/reject withdraw request)
+  const handleConfirm = (id) => {
+    handleOpen()
+  }
+
   // Approve/Reject handler to update the status
   const handleAction = async (id, action) => {
     try {
       const response = await fetch(
-        `${baseUrl}/update-deposit-status/${id}`,
+        `${baseUrl}/update-withdraw-status/${id}`,
         {
           method: "PUT",
           headers: {
@@ -100,7 +110,7 @@ const AdminDepositRequests = () => {
         }
       );
       await response.json();
-      fetchDepositData(); // Reload data after the status update
+      fetchWithdrawData(); // Reload data after the status update
     } catch (err) {
       console.error("Error when updating status", err);
     }
@@ -116,10 +126,12 @@ const AdminDepositRequests = () => {
       {/* Navbar */}
       <AdminNavbar />
 
-      {/* Deposit Requests Heading */}
-      <Typography variant="h5" textAlign="center" sx={{mt: 5}}>Deposit Requests</Typography>
+      {/* Withdraw Requests Heading */}
+      <Typography variant="h5" textAlign="center" sx={{ mt: 5 }}>
+        Withdraw Requests
+      </Typography>
 
-      {/* Deposit Summary Table */}
+      {/* Withdraw Summary Table */}
       <Box>
         {/* Radio Buttons */}
         <RadioGroup row value={status} onChange={handleStatusChange} sx={{mt: 4, ml: 3, mb: 1}}>
@@ -147,25 +159,25 @@ const AdminDepositRequests = () => {
               <TableRow>
                 <TableCell>Ref. No.</TableCell>
                 <TableCell>Investor</TableCell>
-                <TableCell>Deposit Date</TableCell>
-                <TableCell>Deposit Type</TableCell>
-                <TableCell>Deposit Amount</TableCell>
+                <TableCell>Withdraw Date</TableCell>
+                <TableCell>Withdraw Type</TableCell>
+                <TableCell>Withdraw Amount</TableCell>
                 <TableCell>Status</TableCell>
                 {status === 'pending' && <TableCell>Action</TableCell>}
               </TableRow>
             </TableHead>
 
             <TableBody>
-              {depositData.map((item) => (
-                <TableRow key={item?.deposit_transaction_id}>
-                  <TableCell>{item?.deposit_transaction_id}</TableCell>
+              {withdrawData.map((item) => (
+                <TableRow key={item?.withdraw_transaction_id}>
+                  <TableCell>{item?.withdraw_transaction_id}</TableCell>
                   <TableCell>{item?.investor?.user?.firstName} {item?.investor?.user?.lastName}</TableCell>
-                  <TableCell>{item?.depositDate.slice(0, 10)}</TableCell>
+                  <TableCell>{item?.withdrawDate.slice(0, 10)}</TableCell>
                   <TableCell>
-                    {item?.depositType.charAt(0) +
-                      item?.depositType.slice(1).toLowerCase()}
+                    {item?.withdrawType.charAt(0) +
+                      item?.withdrawType.slice(1).toLowerCase()}
                   </TableCell>
-                  <TableCell>{item?.depositAmount}</TableCell>
+                  <TableCell>{item?.withdrawalAmount}</TableCell>
                   <TableCell>
                     {item?.status.charAt(0).toUpperCase() +
                       item?.status.slice(1)}
@@ -200,4 +212,4 @@ const AdminDepositRequests = () => {
   );
 };
 
-export default AdminDepositRequests;
+export default AdminWithdrawRequests;
